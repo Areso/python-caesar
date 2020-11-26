8f8411f31ddaa77187a35d99f58e2ae544e931ec
Anton Gladyshev <returnstrike@yandex.ru>
2020-11-26 22:41:33 +0300

    decoding is done

---
+++ dec.py
@@ -0,0 +1,53 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("shift", help="shift numberic value", type=int)
+parser.add_argument("source", help="source for decoding: file, input, batch, batchf")
+parser.add_argument("-p", "--path", help="name of the file")
+parser.add_argument("-b", "--batch", help="batch string to encode")
+parser.add_argument("-bf", "--batchf", help="batch string to encode. out to file")
+parser.add_argument("-i", "--input", help="interactive input")
+args  = parser.parse_args()
+shift = args.shift
+
+if args.path:
+    myfiler = open(args.path, 'r')
+    content = myfiler.read()
+    myfiler.close()
+if args.batch:
+    content = args.batch
+if args.batchf:
+    content = args.batchf
+if args.input:
+    content = input('Input string: ')
+
+strs = 'abcdefghijklmnopqrstuvwxyz'
+strs = strs + 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+strs = strs + ' ' + strs.upper()
+
+charset_size = len(strs)
+data = ''
+
+for i in content:
+    if i.strip() and i in strs:
+        if strs.index(i) >=shift:
+            orig = strs.index(i) - shift
+        else:
+            orig = strs.index(i) + charset_size - shift
+        data = data + strs[orig]
+    else:
+        data = data + i
+output = data
+
+if (args.batch or args.input):
+    print(output)
+
+if args.batchf:
+    myfileo = open('enc_res.txt', 'w')
+    myfileo.write(output)
+    myfileo.close()
+
+if args.path:
+    outputf = 'out_'+args.path
+    myfileo = open(outputf, 'w')
+    myfileo.write(output)
+    myfileo.close()
